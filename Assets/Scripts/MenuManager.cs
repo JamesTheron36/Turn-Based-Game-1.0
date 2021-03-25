dc28a09ff6f9dd819e0ff5ec096e3af54eb8230f
JamesTheron36 <80755975+JamesTheron36@users.noreply.github.com>
2021-03-25 13:47:45 +0200

    Build 1.0

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public Text critHit;
+    public Text superEff;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,13 @@ public class MenuManager : MonoBehaviour
         
     }
 
+
+    public void CritHit()
+    {
+        critHit.gameObject.SetActive(true);
+        StartCoroutine(CritHitDelay());
+    }
+    
     public void LoadGame()
     {
         SceneManager.LoadScene("Game");
@@ -28,5 +38,11 @@ public class MenuManager : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Menu");
+    }
+    IEnumerator CritHitDelay()
+    {
+        yield return new WaitForSeconds(2);
+        critHit.gameObject.SetActive(false);
+
     }
 }
